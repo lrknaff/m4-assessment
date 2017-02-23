486f7d8b9e7d0886c4464abb3c47e816a95fc4fe
lrknaff <lacey.knaff@gmail.com>
2017-02-23 12:16:36 -0700

    add sass middleware to server

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express        = require('express');
+const path           = require('path');
+const bodyParser     = require('body-parser');
+const connect        = require('connect');
+const sassMiddleware = require('node-sass-middleware');
+const app            = express();
 
-const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'Grudge Box';
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(
+     sassMiddleware({
+         src: __dirname + '/sass',
+         dest: __dirname + '/public/styles',
+         prefix:  '/styles',
+         debug: true
+     })
+ );
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on port 3000`)
