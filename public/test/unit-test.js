df42bf911ca472c5635543f8763331ce8ac0547a
lrknaff <lacey.knaff@gmail.com>
2017-02-27 13:46:43 -0700

    add 3 unit tests

--- public/test/unit-test.js
+++ public/test/unit-test.js
@@ -1,48 +1,62 @@
 describe('Presentation and Interaction', function() {
-  it('can handle receiving name and id', function() {
-    var name = 'Anthony'
-
-  });
-
-  it('sortByName()', function() {
-    const grudgeArray = [
-  {
-    id: 1,
-    name: 'lacey',
-    offense: 'being mean',
-    created_at: '2016-09-10T16:44:28.015Z'
-  },
-  {
-    id: 2,
-    username: 'mike',
-    offense: 'forgetting the cookies',
-    created_at: '2015-09-10T16:44:28.015Z'
-  },
-  {
-    id: 3,
-    username: 'trump',
-    offense: 'having bad hair',
-    created_at: '2014-09-10T16:44:28.015Z'
-  }
+  const grudgeArray = [
+{
+  id: 1,
+  name: 'lacey',
+  forgiven: false,
+  offense: 'being mean',
+  created_at: '2016-09-10T16:44:28.015Z'
+},
+{
+  id: 2,
+  name: 'mike',
+  forgiven: true,
+  offense: 'forgetting the cookies',
+  created_at: '2015-09-10T16:44:28.015Z'
+},
+{
+  id: 3,
+  name: 'trump',
+  forgiven: false,
+  offense: 'having bad hair',
+  created_at: '2014-09-10T16:44:28.015Z'
+}
 ];
-  })
+
+it('sortName()', function() {
+let sortedArray = sortName(grudgeArray);
+
+expect(sortedArray).to.eql([
+    {
+      id: 3,
+      name: 'trump',
+      forgiven: false,
+      offense: 'having bad hair',
+      created_at: '2014-09-10T16:44:28.015Z'
+    },
+    {
+      id: 2,
+      name: 'mike',
+      forgiven: true,
+      offense: 'forgetting the cookies',
+      created_at: '2015-09-10T16:44:28.015Z'
+    },
+    {
+      id: 1,
+      name: 'lacey',
+      forgiven: false,
+      offense: 'being mean',
+      created_at: '2016-09-10T16:44:28.015Z'
+    }
+  ]);
 });
 
-
-describe('Data Management', function() {
-  it('', function() {
-
-  })
+it('countGrudges() count unforgiven', function() {
+  expect(countGrudges(grudgeArray, true)).to.equal(1);
 });
 
-describe('Application State', function() {
-  it('', function() {
-
-  })
+it('countGrudges() count forgiven', function() {
+  expect(countGrudges(grudgeArray, false)).to.equal(2);
 });
 
-describe('Glue', function() {
-  it('', function() {
-
-  })
 });
